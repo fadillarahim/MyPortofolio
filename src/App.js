54d80f09814e207ae1d8cba3b743588bb3241026
fadillarahim <fadillarahimximipa1@gmail.com>
2023-07-29 16:35:44 +0700

    First commit project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react'; 
+import Aos from 'aos';
+
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-indigo-900 text-2xl font-[500]">Halloo Guyss</h1>
+    
   );
 }
 
