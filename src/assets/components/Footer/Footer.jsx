caf7654eaca8cb5e2a4432f7abff887c2f4384a7
fadillarahim <fadillarahimximipa1@gmail.com>
2023-07-30 19:48:02 +0700

    Edit Contact and create footer

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer className='bg-[#12141e] pt-12'>
+      {/* {footer top} */}
+      <div className="container">
+        <div className="sm:flex items-center justify-between md:gap-8">
+          <div className="w-full sm:w-1/2">
+            <h2 className="text-[26px] leading-10 text-white font-[600] mb-5 md:text-[2rem]">
+              
+            </h2>
+          </div>
+        </div>
+      </div>
+      {/* {footer top end} */}
+
+    </footer>
   )
 }
 
