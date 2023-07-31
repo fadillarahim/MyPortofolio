55a71a64adb68d45798b6408aaa23069e009deb1
fadillarahim <fadillarahimximipa1@gmail.com>
2023-07-31 15:14:11 +0700

    All done except contact feature and portofolio still dummy

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 
 function Footer() {
+
+  const year = new Date().getFullYear()
+
   return (
     <footer className='bg-[#12141e] pt-12'>
       {/* {footer top} */}
@@ -9,12 +12,88 @@ function Footer() {
         <div className="sm:flex items-center justify-between md:gap-8">
           <div className="w-full sm:w-1/2">
             <h2 className="text-[26px] leading-10 text-white font-[600] mb-5 md:text-[2rem]">
-              
+              Do you want to make beutiful products?
             </h2>
+            <a href="#contact">
+              <button className='bg-primaryColor text-white font-[500] flex items-center gap-2 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px]'>
+                <i class="ri-mail-line"></i> Hire Me
+              </button>
+            </a>
           </div>
+
+          <div className="w-full sm:w-1/2">
+            <p className="text-gray-300 leading-7 mt-4 sm:mt-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia soluta esse blanditiis, praesentium obcaecati animi, modi officiis corporis eligendi laboriosam iste. 
+            </p>
+
+            <div className="flex items-center gap-4 flex-wrap md:gap-8 mt-10">
+              <span className='text-gray-300 font-[400] text-[15px]'>
+                Follow Me:
+              </span>
+
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a href="https://www.linkedin.com/in/fadilla-rahim-96034621b/" className='text-gray-300 font-[500] text-[18px]' target='blank'>
+                  <i class="ri-linkedin-box-fill"></i>
+                </a>
+              </span>
+
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a href="https://github.com/fadillarahim" className='text-gray-300 font-[500] text-[18px]' target='blank'>
+                  <i class="ri-github-fill"></i>
+                </a>
+              </span>
+
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a href="https://www.instagram.com/fadilla_rahim/" className='text-gray-300 font-[500] text-[18px]' target='blank'>
+                  <i class="ri-instagram-fill"></i>
+                </a>
+              </span>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="menu">
+          <ul className='flex item-center justify-center gap-10 mt-10'>
+            <li className='text-gray-400 font-[400]'><a href="#about">About</a></li>
+            <li className='text-gray-400 font-[400]'><a href="#services">Services</a></li>
+            <li className='text-gray-400 font-[400]'><a href="#portofolio">Portofolio</a></li>
+            <li className='text-gray-400 font-[400]'><a href="#contact">Contact</a></li>
+           </ul>
         </div>
       </div>
       {/* {footer top end} */}
+
+      {/* {Footer Botom} */}
+      <div className="bg-[#1b1e29] py-3 mt-14">
+        <div className="container">
+          <div className="flex items-center justify-center sm:justify-between">
+
+            <div className="hidden sm:block">
+              <div className="flex items-center gap-[10px]">
+                <span className='w-[35px] h-[35px] rounded-full bg-[#2b2d33] text-white font-[500] text-[18px] flex items-center justify-center'>
+                  F
+                </span>
+
+                <div className="leading-[20px]">
+                  <h2 className='text-gray-200 font-[500] text-[18px]'>
+                    Fadil
+                  </h2>
+                  <p className='text-gray-300 text-[14px] font-[500]'>
+                    Personal
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <p className="text-gray-400 text-[14px]">Copyright {year} develop by Fadilla Rahim - All rigth reserved.</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      {/* {Footer Botom End} */}
 
     </footer>
   )
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,9 +1,48 @@
 
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 const Header = () => {
+
+    const headerRef = useRef(null);
+    const menuRef = useRef(null);
+
+    const stickyHeaderFunc = () => {
+        window.addEventListener('scroll', () => {
+            if(
+                document.body.scrollTop > 80 || document.documentElement.scrollTop > 80
+            ) {
+                headerRef.current.classList.add('sticky__header');
+            } else {
+                headerRef.current.classList.remove('sticky__header');
+            }
+        });
+        
+    };
+
+    useEffect(() => {
+        stickyHeaderFunc()
+
+        return window.removeEventListener('scroll', stickyHeaderFunc)
+    }, []);
+
+    const handleClick = e => {
+        e.preventDefault();
+
+        const targetAttr = e.target.getAttribute("href");
+        const location = document.querySelector(targetAttr).offsetTop
+
+        window.scrollTo({
+            top: location - 80,
+            left: 0,
+        });
+    };
+
+    const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return (
-    <header className='w-full h-[80px] leading-[80px] flex items-center'>
+    <header 
+    ref={headerRef} 
+    className='w-full h-[80px] leading-[80px] flex items-center'>
         <div className="container">
             <div className='flex items-center justify-between'>
                 {/* {=========Logo=========} */}
@@ -19,12 +58,32 @@ const Header = () => {
                 </div>
                 {/* {logo end} */}
                 {/* {Menu Start} */}
-                <div className="menu">
+                <div className="menu" ref={menuRef} onClick={toggleMenu} >
                     <ul className='flex item-center gap-10'>
-                        <li className='text-smallTextColor font-[400]'><a href="#about">About</a></li>
-                        <li className='text-smallTextColor font-[400]'><a href="#services">Services</a></li>
-                        <li className='text-smallTextColor font-[400]'><a href="#portofolio">Portofolio</a></li>
-                        <li className='text-smallTextColor font-[400]'><a href="#contact">Contact</a></li>
+                        <li>
+                            <a href="#about" className='text-smallTextColor font-[400]'
+                            onClick={handleClick}>
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#services" className='text-smallTextColor font-[400]'
+                            onClick={handleClick}>
+                                Services
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#portofolio" className='text-smallTextColor font-[400]'
+                            onClick={handleClick}>
+                                Portofolio
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#contact" className='text-smallTextColor font-[400]'
+                            onClick={handleClick}>
+                                Contact
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 {/* {Menu End} */}
@@ -32,11 +91,13 @@ const Header = () => {
                 {/* {Menu Right} */}
                 <div className="flex items-center gap-4">
                     <button className='flex items-center justify-center gap-2 text-smallTextColor font-[400] border border-solid border-smallTextColor px-4 py-2 rounded-[8px] max-h-[50px] hover:bg-smallTextColor hover:text-white hover:font-[300] ease-in duration-300'>
-                        <i class="ri-send-plane-line"></i> Let's Talk
+                        <i className="ri-send-plane-line"></i> Let's Talk
                     </button>
 
-                    <span className='text-2xl text-smallTextColor md:hidden cursor-pointer'>
-                        <i class="ri-menu-line"></i>
+                    <span 
+                    className='text-2xl text-smallTextColor md:hidden cursor-pointer'
+                    onClick={toggleMenu}>
+                        <i className="ri-menu-line"></i>
                     </span>
                 </div>
                 {/* {Menu End} */}
